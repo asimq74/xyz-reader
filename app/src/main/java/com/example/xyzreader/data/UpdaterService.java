7d34480319e9b15a3aed04e9b167b8605e450e4e
Asim Qureshi <aqureshi@geico.com>
2018-09-04 17:39:53 -0400

    removed selection builder (no longer used)

--- app/src/main/java/com/example/xyzreader/data/UpdaterService.java
+++ app/src/main/java/com/example/xyzreader/data/UpdaterService.java
@@ -1,12 +1,12 @@
 package com.example.xyzreader.data;
 
+import java.util.List;
+
 import android.app.IntentService;
 import android.arch.persistence.room.Room;
-import android.content.ContentProviderOperation;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.text.format.Time;
 import android.util.Log;
 import android.widget.Toast;
@@ -14,73 +14,69 @@ import android.widget.Toast;
 import com.example.xyzreader.remote.retrofit.GetBookItems;
 import com.example.xyzreader.remote.retrofit.RetrofitClientInstance;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class UpdaterService extends IntentService {
 
-    public static final String BROADCAST_ACTION_STATE_CHANGE
-            = "com.example.xyzreader.intent.action.STATE_CHANGE";
-    public static final String EXTRA_REFRESHING
-            = "com.example.xyzreader.intent.extra.REFRESHING";
-    private static final String TAG = "UpdaterService";
+	public static final String BROADCAST_ACTION_STATE_CHANGE
+			= "com.example.xyzreader.intent.action.STATE_CHANGE";
+	public static final String EXTRA_REFRESHING
+			= "com.example.xyzreader.intent.extra.REFRESHING";
+	private static final String TAG = "UpdaterService";
 
-    public UpdaterService() {
-        super(TAG);
-    }
+	public UpdaterService() {
+		super(TAG);
+	}
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        Time time = new Time();
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Time time = new Time();
 
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo ni = cm.getActiveNetworkInfo();
-        if (ni == null || !ni.isConnected()) {
-            Log.w(TAG, "Not online, not refreshing.");
-            return;
-        }
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if (ni == null || !ni.isConnected()) {
+			Log.w(TAG, "Not online, not refreshing.");
+			return;
+		}
 
-        sendStickyBroadcast(
-                new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, true));
+		sendStickyBroadcast(
+				new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, true));
 
-        /*Create handle for the RetrofitInstance interface*/
-        GetBookItems service = RetrofitClientInstance.getRetrofitInstance().create(GetBookItems.class);
-        Call<List<BookItem>> call = service.getAllBookItems();
-        call.enqueue(new Callback<List<BookItem>>() {
-            private static final String DATABASE_NAME = "bookitems_db";
-            private BookItemDatabase bookItemDatabase;
+		/*Create handle for the RetrofitInstance interface*/
+		GetBookItems service = RetrofitClientInstance.getRetrofitInstance().create(GetBookItems.class);
+		Call<List<BookItem>> call = service.getAllBookItems();
+		call.enqueue(new Callback<List<BookItem>>() {
+			private static final String DATABASE_NAME = "bookitems_db";
+			private BookItemDatabase bookItemDatabase;
 
-            @Override
-            public void onFailure(Call<List<BookItem>> call, Throwable t) {
-                Log.e(this.getClass().getSimpleName(), "error calling service", t);
-                Toast.makeText(getApplicationContext(), "Something went wrong...Please try later!", Toast.LENGTH_LONG).show();
-            }
+			@Override
+			public void onFailure(Call<List<BookItem>> call, Throwable t) {
+				Log.e(this.getClass().getSimpleName(), "error calling service", t);
+				Toast.makeText(getApplicationContext(), "Something went wrong...Please try later!", Toast.LENGTH_LONG).show();
+			}
 
-            @Override
-            public void onResponse(Call<List<BookItem>> call, Response<List<BookItem>> response) {
-                final List<BookItem> bookItems = response.body();
-                if (bookItems == null || bookItems.size() == 0) {
-                    return;
-                }
+			@Override
+			public void onResponse(Call<List<BookItem>> call, Response<List<BookItem>> response) {
+				final List<BookItem> bookItems = response.body();
+				if (bookItems == null || bookItems.size() == 0) {
+					return;
+				}
 
+				bookItemDatabase = Room.databaseBuilder(getApplicationContext(),
+						BookItemDatabase.class, DATABASE_NAME).fallbackToDestructiveMigration().build();
 
-                bookItemDatabase = Room.databaseBuilder(getApplicationContext(),
-                        BookItemDatabase.class, DATABASE_NAME).fallbackToDestructiveMigration().build();
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						bookItemDatabase.daoAccess().insertMultipleBookItems(bookItems);
+					}
+				}).start();
+			}
+		});
 
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        bookItemDatabase.daoAccess().insertMultipleBookItems(bookItems);
-                    }
-                }).start();
-            }
-        });
-
-        sendStickyBroadcast(
-                new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, false));
-    }
+		sendStickyBroadcast(
+				new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, false));
+	}
 }
