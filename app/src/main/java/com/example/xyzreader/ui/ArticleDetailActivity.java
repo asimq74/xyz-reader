91aafab84dc8e62213feb5613ed664d3b1ca2b9f
Asim Qureshi <aqureshi@geico.com>
2018-09-07 17:34:20 -0400

    added title box and collapsing toolbar on detail fragment; added Glide support

--- app/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ app/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -15,7 +15,10 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.BookItem;
 import com.example.xyzreader.data.SingleBookItemLoader;
@@ -24,151 +27,136 @@ import com.example.xyzreader.data.SingleBookItemLoader;
  * An activity representing a single Article detail screen, letting you swipe between articles.
  */
 public class ArticleDetailActivity extends AppCompatActivity
-        implements LoaderManager.LoaderCallbacks<BookItem> {
+		implements LoaderManager.LoaderCallbacks<BookItem> {
 
-    public static final String BOOK_ITEM = "BOOK_ITEM";
-    private long mStartId;
+	private class MyPagerAdapter extends FragmentStatePagerAdapter {
 
-    private long mSelectedItemId;
-    private int mSelectedItemUpButtonFloor = Integer.MAX_VALUE;
-    private int mTopInset;
+		private final BookItem bookItem;
 
-    private ViewPager mPager;
-    private MyPagerAdapter mPagerAdapter;
-    private View mUpButtonContainer;
-    private View mUpButton;
+		public MyPagerAdapter(FragmentManager fm, @NonNull BookItem bookItem) {
+			super(fm);
+			this.bookItem = bookItem;
+		}
+
+		@Override
+		public int getCount() {
+			return (bookItem != null) ? 1 : 0;
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return ArticleDetailFragment.newInstance(bookItem);
+		}
+
+		@Override
+		public void setPrimaryItem(ViewGroup container, int position, Object object) {
+			super.setPrimaryItem(container, position, object);
+			ArticleDetailFragment fragment = (ArticleDetailFragment) object;
+		}
+	}
+	public static final String BOOK_ITEM = "BOOK_ITEM";
+	private ViewPager mPager;
+	private MyPagerAdapter mPagerAdapter;
+	private long mSelectedItemId;
+	private long mStartId;
+	private int mTopInset;
+	private View mUpButton;
+	private View mUpButtonContainer;
+	private View toolbar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			getWindow().getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+							View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		}
+		setContentView(R.layout.activity_article_detail);
+		if (savedInstanceState == null) {
+			if (getIntent() != null && getIntent().getExtras() != null) {
+				mStartId = getIntent().getLongExtra(ArticleListActivity.ITEM_ID, 0);
+				mSelectedItemId = mStartId;
+			}
+		}
+		prepareLoader(24);
+
+		mUpButtonContainer = findViewById(R.id.up_container);
+
+		mUpButton = findViewById(R.id.action_up);
+		mUpButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				onSupportNavigateUp();
+			}
+		});
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			mUpButtonContainer.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+				@Override
+				public WindowInsets onApplyWindowInsets(View view, WindowInsets windowInsets) {
+					view.onApplyWindowInsets(windowInsets);
+					mTopInset = windowInsets.getSystemWindowInsetTop();
+					mUpButtonContainer.setTranslationY(mTopInset);
+					return windowInsets;
+				}
+			});
+		}
+
+	}
+
+	@Override
+	public Loader<BookItem> onCreateLoader(int i, Bundle bundle) {
+		return SingleBookItemLoader.newBookItemLoaderInstance(this, mStartId);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<BookItem> loader, BookItem data) {
+		toolbar = findViewById(R.id.toolbar);
+		Glide.with(this)
+				.load(data.getThumb())
+				.into((ImageView) findViewById(R.id.photo));
+		TextView title = findViewById(R.id.title);
+		title.setText(data.getTitle());
+		TextView author = findViewById(R.id.author);
+		author.setText(data.getAuthor());
+		mPagerAdapter = new MyPagerAdapter(getFragmentManager(), data);
+		mPagerAdapter.notifyDataSetChanged();
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPager.setAdapter(mPagerAdapter);
+		mPager.setPageMargin((int) TypedValue
+				.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
+		mPager.setPageMarginDrawable(new ColorDrawable(0x22000000));
+
+		mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			@Override
+			public void onPageScrollStateChanged(int state) {
+				super.onPageScrollStateChanged(state);
+				mUpButton.animate()
+						.alpha((state == ViewPager.SCROLL_STATE_IDLE) ? 1f : 0f)
+						.setDuration(300);
+			}
+
+		});
+
+		// Select the start ID
+		mPager.setCurrentItem(0, false);
+
+		mStartId = 0;
+	}
+
+	@Override
+	public void onLoaderReset(Loader<BookItem> loader) {
+		mPagerAdapter.notifyDataSetChanged();
+	}
 
 
-    protected void prepareLoader(@NonNull final int loaderId) {
-        if (getSupportLoaderManager().getLoader(loaderId) == null) {
-            getSupportLoaderManager().initLoader(loaderId, null, this).forceLoad();
-            return;
-        }
-        getSupportLoaderManager().restartLoader(loaderId, null, this).forceLoad();
-    }
+	protected void prepareLoader(@NonNull final int loaderId) {
+		if (getSupportLoaderManager().getLoader(loaderId) == null) {
+			getSupportLoaderManager().initLoader(loaderId, null, this).forceLoad();
+			return;
+		}
+		getSupportLoaderManager().restartLoader(loaderId, null, this).forceLoad();
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        }
-        setContentView(R.layout.activity_article_detail);
-        if (savedInstanceState == null) {
-            if (getIntent() != null && getIntent().getExtras() != null) {
-                mStartId = getIntent().getLongExtra(ArticleListActivity.ITEM_ID, 0);
-                mSelectedItemId = mStartId;
-            }
-        }
-        prepareLoader(24);
-
-        mUpButtonContainer = findViewById(R.id.up_container);
-
-        mUpButton = findViewById(R.id.action_up);
-        mUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onSupportNavigateUp();
-            }
-        });
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            mUpButtonContainer.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
-                @Override
-                public WindowInsets onApplyWindowInsets(View view, WindowInsets windowInsets) {
-                    view.onApplyWindowInsets(windowInsets);
-                    mTopInset = windowInsets.getSystemWindowInsetTop();
-                    mUpButtonContainer.setTranslationY(mTopInset);
-                    updateUpButtonPosition();
-                    return windowInsets;
-                }
-            });
-        }
-
-    }
-
-    @Override
-    public void onLoadFinished(Loader<BookItem> loader, BookItem data) {
-        mPagerAdapter = new MyPagerAdapter(getFragmentManager(), data);
-        mPagerAdapter.notifyDataSetChanged();
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPager.setAdapter(mPagerAdapter);
-        mPager.setPageMargin((int) TypedValue
-                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
-        mPager.setPageMarginDrawable(new ColorDrawable(0x22000000));
-
-        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                super.onPageScrollStateChanged(state);
-                mUpButton.animate()
-                        .alpha((state == ViewPager.SCROLL_STATE_IDLE) ? 1f : 0f)
-                        .setDuration(300);
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                updateUpButtonPosition();
-            }
-        });
-
-        // Select the start ID
-        mPager.setCurrentItem(0, false);
-
-        mStartId = 0;
-    }
-
-    @Override
-    public void onLoaderReset(Loader<BookItem> loader) {
-        mPagerAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public Loader<BookItem> onCreateLoader(int i, Bundle bundle) {
-        return SingleBookItemLoader.newBookItemLoaderInstance(this, mStartId);
-    }
-
-
-    public void onUpButtonFloorChanged(long itemId, ArticleDetailFragment fragment) {
-        if (itemId == mSelectedItemId) {
-            mSelectedItemUpButtonFloor = fragment.getUpButtonFloor();
-            updateUpButtonPosition();
-        }
-    }
-
-    private void updateUpButtonPosition() {
-        int upButtonNormalBottom = mTopInset + mUpButton.getHeight();
-        mUpButton.setTranslationY(Math.min(mSelectedItemUpButtonFloor - upButtonNormalBottom, 0));
-    }
-
-    private class MyPagerAdapter extends FragmentStatePagerAdapter {
-        private final BookItem bookItem;
-
-        public MyPagerAdapter(FragmentManager fm, @NonNull BookItem bookItem) {
-            super(fm);
-            this.bookItem = bookItem;
-        }
-
-        @Override
-        public void setPrimaryItem(ViewGroup container, int position, Object object) {
-            super.setPrimaryItem(container, position, object);
-            ArticleDetailFragment fragment = (ArticleDetailFragment) object;
-            if (fragment != null) {
-                mSelectedItemUpButtonFloor = fragment.getUpButtonFloor();
-                updateUpButtonPosition();
-            }
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return ArticleDetailFragment.newInstance(bookItem);
-        }
-
-        @Override
-        public int getCount() {
-            return (bookItem != null) ? 1 : 0;
-        }
-    }
 }
--- app/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ app/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -1,15 +1,17 @@
 package com.example.xyzreader.ui;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import android.app.Fragment;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
-import android.support.v7.graphics.Palette;
 import android.text.Html;
 import android.text.format.DateUtils;
 import android.text.method.LinkMovementMethod;
@@ -17,18 +19,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.BookItem;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 /**
  * A fragment representing a single Article detail screen. This fragment is
@@ -48,8 +42,6 @@ public class ArticleDetailFragment extends Fragment {
     private ColorDrawable mStatusBarColorDrawable;
 
     private int mTopInset;
-    private View mPhotoContainerView;
-    private ImageView mPhotoView;
     private int mScrollY;
     private boolean mIsCard = false;
     private int mStatusBarFullOpacityBottom;
@@ -127,14 +119,8 @@ public class ArticleDetailFragment extends Fragment {
             @Override
             public void onScrollChanged() {
                 mScrollY = mScrollView.getScrollY();
-                getActivityCast().onUpButtonFloorChanged(mItemId, ArticleDetailFragment.this);
-                mPhotoContainerView.setTranslationY((int) (mScrollY - mScrollY / PARALLAX_FACTOR));
-                updateStatusBar();
             }
         });
-
-        mPhotoView = (ImageView) mRootView.findViewById(R.id.photo);
-        mPhotoContainerView = mRootView.findViewById(R.id.photo_container);
 
         mStatusBarColorDrawable = new ColorDrawable(0);
 
@@ -149,24 +135,9 @@ public class ArticleDetailFragment extends Fragment {
         });
 
         bindViews();
-        updateStatusBar();
         return mRootView;
     }
 
-    private void updateStatusBar() {
-        int color = 0;
-        if (mPhotoView != null && mTopInset != 0 && mScrollY > 0) {
-            float f = progress(mScrollY,
-                    mStatusBarFullOpacityBottom - mTopInset * 3,
-                    mStatusBarFullOpacityBottom - mTopInset);
-            color = Color.argb((int) (255 * f),
-                    (int) (Color.red(mMutedColor) * 0.9),
-                    (int) (Color.green(mMutedColor) * 0.9),
-                    (int) (Color.blue(mMutedColor) * 0.9));
-        }
-        mStatusBarColorDrawable.setColor(color);
-        mDrawInsetsFrameLayout.setInsetBackground(mStatusBarColorDrawable);
-    }
 
     private Date parsePublishedDate() {
         try {
@@ -189,7 +160,6 @@ public class ArticleDetailFragment extends Fragment {
         bylineView.setMovementMethod(new LinkMovementMethod());
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
 
-
         bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
 
         if (bookItem != null) {
@@ -200,62 +170,27 @@ public class ArticleDetailFragment extends Fragment {
             Date publishedDate = parsePublishedDate();
             if (!publishedDate.before(START_OF_EPOCH.getTime())) {
                 bylineView.setText(Html.fromHtml(
-                        DateUtils.getRelativeTimeSpanString(
-                                publishedDate.getTime(),
-                                System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
-                                DateUtils.FORMAT_ABBREV_ALL).toString()
-                                + " by <font color='#ffffff'>"
-                                + bookItem.getAuthor()
-                                + "</font>"));
+                    DateUtils.getRelativeTimeSpanString(
+                        publishedDate.getTime(),
+                        System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                        DateUtils.FORMAT_ABBREV_ALL).toString()
+                        + " by <font color='#ffffff'>"
+                        + bookItem.getAuthor()
+                        + "</font>"));
 
             } else {
                 // If date is before 1902, just show the string
                 bylineView.setText(Html.fromHtml(
-                        outputFormat.format(publishedDate) + " by <font color='#ffffff'>"
-                                + bookItem.getAuthor()
-                                + "</font>"));
+                    outputFormat.format(publishedDate) + " by <font color='#ffffff'>"
+                        + bookItem.getAuthor()
+                        + "</font>"));
 
             }
             final String bodyRawText = bookItem.getBody();
             bodyView.setText(Html.fromHtml(bodyRawText
-                    .replaceAll("(\r\n\r\n)", "<p />")
-                    .replaceAll("(\r\n)", " ")));
-            ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
-                    .get(bookItem.getPhoto(), new ImageLoader.ImageListener() {
-                        @Override
-                        public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
-                            Bitmap bitmap = imageContainer.getBitmap();
-                            if (bitmap != null) {
-                                Palette p = Palette.generate(bitmap, 12);
-                                mMutedColor = p.getDarkMutedColor(0xFF333333);
-                                mPhotoView.setImageBitmap(imageContainer.getBitmap());
-                                mRootView.findViewById(R.id.meta_bar)
-                                        .setBackgroundColor(mMutedColor);
-                                updateStatusBar();
-                            }
-                        }
-
-                        @Override
-                        public void onErrorResponse(VolleyError volleyError) {
-
-                        }
-                    });
-        } else {
-            mRootView.setVisibility(View.GONE);
-            titleView.setText("N/A");
-            bylineView.setText("N/A");
-            bodyView.setText("N/A");
-        }
-    }
-
-    public int getUpButtonFloor() {
-        if (mPhotoContainerView == null || mPhotoView.getHeight() == 0) {
-            return Integer.MAX_VALUE;
+                .replaceAll("(\r\n\r\n)", "<p />")
+                .replaceAll("(\r\n)", " ")));
         }
 
-        // account for parallax
-        return mIsCard
-                ? (int) mPhotoContainerView.getTranslationY() + mPhotoView.getHeight() - mScrollY
-                : mPhotoView.getHeight() - mScrollY;
     }
 }
