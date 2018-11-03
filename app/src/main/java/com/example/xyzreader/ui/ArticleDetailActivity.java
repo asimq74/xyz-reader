9ea1eb2d54f37ab729cd4af17c52ffde775c7a4a
asimq74 <asim.m.qureshi@gmail.com>
2018-11-03 16:44:42 -0400

    added refresh support

--- app/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ app/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -1,12 +1,5 @@
 package com.example.xyzreader.ui;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import javax.inject.Inject;
-
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.content.res.ColorStateList;
@@ -42,6 +35,13 @@ import com.example.xyzreader.viewmodels.BookItemViewModel;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.inject.Inject;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -50,201 +50,206 @@ import butterknife.OnClick;
  * An activity representing a single Article detail screen, letting you swipe between articles.
  */
 public class ArticleDetailActivity extends AppCompatActivity
-		implements AppBarLayout.OnOffsetChangedListener {
+        implements AppBarLayout.OnOffsetChangedListener {
 
-	public class SnackBarListener implements View.OnClickListener {
+    private final String TAG = this.getClass().getSimpleName();
+    @BindView(R.id.app_bar_layout)
+    AppBarLayout appBarLayout;
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.float_header_view)
+    HeaderView floatHeaderView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.toolbar_header_view)
+    HeaderView toolbarHeaderView;
+    @Inject
+    ProjectViewModelFactory viewModelFactory;
+    private String publishedDate;
+    private CardView cardView;
+    private BookItem data;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss", Locale.US);
+    private boolean isHideToolbarView = false;
+    private long mStartId;
+    private BookItemViewModel viewModel;
+    private SnackBarListener snackBarListener = new SnackBarListener();;
 
-		@Override
-		public void onClick(View v) {
-			Uri pictureUri = Uri.parse(data.getPhoto());
-			Intent shareIntent = new Intent();
-			shareIntent.setAction(Intent.ACTION_SEND);
-			shareIntent.putExtra(Intent.EXTRA_TEXT, data.getAuthor());
-			shareIntent.putExtra(Intent.EXTRA_STREAM, pictureUri);
-			shareIntent.setType("image/*");
-			shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-			startActivity(Intent.createChooser(shareIntent, "Share images..."));
-		}
-	}
+    private void applyPalette(Palette palette) {
+        int primaryDark = getResources().getColor(R.color.theme_primary_dark);
+        int primary = getResources().getColor(R.color.theme_primary);
+        collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
+        collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
+        updateBackground(findViewById(R.id.fab), palette);
+        supportStartPostponedEnterTransition();
+    }
 
-	private final String TAG = this.getClass().getSimpleName();
-	@BindView(R.id.app_bar_layout)
-	AppBarLayout appBarLayout;
-	private CardView cardView;
-	@BindView(R.id.collapsing_toolbar)
-	CollapsingToolbarLayout collapsingToolbarLayout;
-	private BookItem data;
-	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss", Locale.US);
-	@BindView(R.id.float_header_view)
-	HeaderView floatHeaderView;
-	private boolean isHideToolbarView = false;
-	private long mStartId;
-	@BindView(R.id.progressBar)
-	ProgressBar progressBar;
-	@BindView(R.id.toolbar)
-	Toolbar toolbar;
-	@BindView(R.id.toolbar_header_view)
-	HeaderView toolbarHeaderView;
-	private BookItemViewModel viewModel;
-	@Inject
-	ProjectViewModelFactory viewModelFactory;
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent motionEvent) {
+        try {
+            return super.dispatchTouchEvent(motionEvent);
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
 
-	private void applyPalette(Palette palette) {
-		int primaryDark = getResources().getColor(R.color.theme_primary_dark);
-		int primary = getResources().getColor(R.color.theme_primary);
-		collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
-		collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
-		updateBackground(findViewById(R.id.fab), palette);
-		supportStartPostponedEnterTransition();
-	}
+    @OnClick(R.id.fab)
+    public void onClickFab(View view) {
+        Snackbar mySnackbar = Snackbar.make(view, R.string.thanksForSharing, Snackbar.LENGTH_LONG);
+        mySnackbar.setAction(R.string.shareArticle, snackBarListener);
+        TextView text = mySnackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+        text.setTextColor(getResources().getColor(R.color.ltgray));
+        mySnackbar.setActionTextColor(Color.WHITE);
+        mySnackbar.show();
+    }
 
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent motionEvent) {
-		try {
-			return super.dispatchTouchEvent(motionEvent);
-		} catch (NullPointerException e) {
-			return false;
-		}
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_detail);
+        ButterKnife.bind(this);
+        final MyApplication application = (MyApplication) getApplicationContext();
+        application.getApplicationComponent().inject(this);
+        collapsingToolbarLayout.setTitle(" ");
+        cardView = findViewById(R.id.cardView);
+        appBarLayout = findViewById(R.id.app_bar_layout);
+        appBarLayout.addOnOffsetChangedListener(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if (savedInstanceState == null) {
+            if (getIntent() != null && getIntent().getExtras() != null) {
+                mStartId = getIntent().getLongExtra(ArticleListActivity.ITEM_ID, 0);
+            }
+        } else {
+            mStartId = savedInstanceState.getLong(ArticleListActivity.ITEM_ID);
+        }
+    }
 
-	@OnClick(R.id.fab)
-	public void onClickFab(View view) {
-		Snackbar mySnackbar = Snackbar.make(view,
-				"Here's a Snackbar", Snackbar.LENGTH_LONG);
-		mySnackbar.setAction("Action", new SnackBarListener());
-		TextView text = mySnackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
-		text.setTextColor(getResources().getColor(R.color.ltgray));
-		mySnackbar.setActionTextColor(Color.WHITE);
-		mySnackbar.show();
-	}
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(offset) / (float) maxScroll;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_article_detail);
-		ButterKnife.bind(this);
-		final MyApplication application = (MyApplication) getApplicationContext();
-		application.getApplicationComponent().inject(this);
-		collapsingToolbarLayout.setTitle(" ");
-		cardView = findViewById(R.id.cardView);
-		appBarLayout = findViewById(R.id.app_bar_layout);
-		appBarLayout.addOnOffsetChangedListener(this);
-		setSupportActionBar(toolbar);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		if (savedInstanceState == null) {
-			if (getIntent() != null && getIntent().getExtras() != null) {
-				mStartId = getIntent().getLongExtra(ArticleListActivity.ITEM_ID, 0);
-			}
-		} else {
-			mStartId = savedInstanceState.getLong(ArticleListActivity.ITEM_ID);
-		}
-	}
+        if (percentage == 1f && isHideToolbarView) {
+            toolbarHeaderView.setVisibility(View.VISIBLE);
+            toolbarHeaderView.findViewById(R.id.header_view_author).setVisibility(View.GONE);
+            toolbarHeaderView.findViewById(R.id.header_view_published_date).setVisibility(View.GONE);
+            final TextView titleView = toolbarHeaderView.findViewById(R.id.header_view_title);
+            titleView.setTextAppearance(this,
+                    getResources().getBoolean(R.bool.isTablet) ?
+                            android.R.style.TextAppearance_Material_Headline :
+                            android.R.style.TextAppearance_Material_Subhead);
+            titleView.setTextColor(ContextCompat.getColor(this, android.R.color.white));
+            isHideToolbarView = !isHideToolbarView;
 
-	@Override
-	public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
-		int maxScroll = appBarLayout.getTotalScrollRange();
-		float percentage = (float) Math.abs(offset) / (float) maxScroll;
+        } else if (percentage < 1f && !isHideToolbarView) {
+            toolbarHeaderView.setVisibility(View.GONE);
+            isHideToolbarView = !isHideToolbarView;
+        }
+    }
 
-		if (percentage == 1f && isHideToolbarView) {
-			toolbarHeaderView.setVisibility(View.VISIBLE);
-			toolbarHeaderView.findViewById(R.id.header_view_author).setVisibility(View.GONE);
-			toolbarHeaderView.findViewById(R.id.header_view_published_date).setVisibility(View.GONE);
-			final TextView titleView = toolbarHeaderView.findViewById(R.id.header_view_title);
-			titleView.setTextAppearance(this, android.R.style.TextAppearance_Material_Subhead);
-			titleView.setTextColor(ContextCompat.getColor(this, android.R.color.white));
-			isHideToolbarView = !isHideToolbarView;
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putLong(ArticleListActivity.ITEM_ID, mStartId);
+        super.onSaveInstanceState(outState);
+    }
 
-		} else if (percentage < 1f && !isHideToolbarView) {
-			toolbarHeaderView.setVisibility(View.GONE);
-			isHideToolbarView = !isHideToolbarView;
-		}
-	}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(BookItemViewModel.class);
+        final int bookId = (int) this.mStartId;
+        viewModel.getBookItem(bookId).observe(this, bookItem -> populateInitialView(bookItem));
+        viewModel.getBody(bookId).observe(this, body -> populateBody(body));
+    }
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		outState.putLong(ArticleListActivity.ITEM_ID, mStartId);
-		super.onSaveInstanceState(outState);
-	}
+    private Date parsePublishedDate(String dateString) {
+        try {
+            return dateFormat.parse(dateString);
+        } catch (ParseException ex) {
+            Log.e(TAG, ex.getMessage());
+            Log.i(TAG, "passing today's date");
+            return new Date();
+        }
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		viewModel = ViewModelProviders.of(this, viewModelFactory)
-				.get(BookItemViewModel.class);
-		final int bookId = (int) this.mStartId;
-		viewModel.getBookItem(bookId).observe(this, bookItem -> populateInitialView(bookItem));
-		viewModel.getBody(bookId).observe(this, body -> populateBody(body));
-//		viewModel.getHeaderLoaded().observe(this, headerLoaded -> populateBody(data.getBody()));
-	}
+    protected void populateBody(String body) {
+        long start = System.currentTimeMillis();
+        cardView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        final TextView articleBodyView = findViewById(R.id.article_body);
+        articleBodyView.setText(Html.fromHtml(body
+                .replaceAll("(\r\n\r\n)", "<p />")
+                .replaceAll("(\r\n)", " ")));
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        Log.i(TAG, "timeElapsed to populate body: " + timeElapsed + " mS");
+        progressBar.setVisibility(View.GONE);
+        cardView.setVisibility(View.VISIBLE);
+    }
 
-	private Date parsePublishedDate(String dateString) {
-		try {
-			return dateFormat.parse(dateString);
-		} catch (ParseException ex) {
-			Log.e(TAG, ex.getMessage());
-			Log.i(TAG, "passing today's date");
-			return new Date();
-		}
-	}
+    private void populateInitialView(@NonNull BookItem bookItem) {
+        if (bookItem != null) {
+            long start = System.currentTimeMillis();
+            data = bookItem;
+            publishedDate = DateUtils.getRelativeTimeSpanString(
+                    parsePublishedDate(bookItem.getPublishedDate()).getTime(),
+                    System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                    DateUtils.FORMAT_ABBREV_ALL).toString();
+            toolbarHeaderView.bindTo(bookItem.getTitle(), bookItem.getAuthor(), publishedDate);
+            floatHeaderView.bindTo(bookItem.getTitle(), bookItem.getAuthor(), publishedDate);
+            final ImageView photoView = findViewById(R.id.photo);
+            final String photoUrl = bookItem.getPhoto();
+            Picasso.with(ArticleDetailActivity.this).load(photoUrl).into(photoView, new Callback() {
+                @Override
+                public void onError() {
+                    Log.e(TAG, "error loading photoView with url: " + photoUrl);
+                }
 
-	protected void populateBody(String body) {
-		long start = System.currentTimeMillis();
-		cardView.setVisibility(View.GONE);
-		progressBar.setVisibility(View.VISIBLE);
-		final TextView articleBodyView = findViewById(R.id.article_body);
-		articleBodyView.setText(Html.fromHtml(body
-				.replaceAll("(\r\n\r\n)", "<p />")
-				.replaceAll("(\r\n)", " ")));
-		long finish = System.currentTimeMillis();
-		long timeElapsed = finish - start;
-		Log.i(TAG, "timeElapsed to populate body: " + timeElapsed + " mS");
-		progressBar.setVisibility(View.GONE);
-		cardView.setVisibility(View.VISIBLE);
-	}
+                private void onGenerated(Palette palette) {
+                    applyPalette(palette);
+                }
 
-	private void populateInitialView(@NonNull BookItem bookItem) {
-		if (bookItem != null) {
-			long start = System.currentTimeMillis();
-			data = bookItem;
-			final String publishedDate = DateUtils.getRelativeTimeSpanString(
-					parsePublishedDate(bookItem.getPublishedDate()).getTime(),
-					System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
-					DateUtils.FORMAT_ABBREV_ALL).toString();
-			toolbarHeaderView.bindTo(bookItem.getTitle(), bookItem.getAuthor(), publishedDate);
-			floatHeaderView.bindTo(bookItem.getTitle(), bookItem.getAuthor(), publishedDate);
-			final ImageView photoView = findViewById(R.id.photo);
-			final String photoUrl = bookItem.getPhoto();
-			Picasso.with(ArticleDetailActivity.this).load(photoUrl).into(photoView, new Callback() {
-				@Override
-				public void onError() {
-					Log.e(TAG, "error loading photoView with url: " + photoUrl);
-				}
+                @Override
+                public void onSuccess() {
+                    Bitmap bitmap = ((BitmapDrawable) photoView.getDrawable()).getBitmap();
+                    Palette.from(bitmap).generate(this::onGenerated);
+                    viewModel.getBody(data.getId()).observe(ArticleDetailActivity.this, body -> populateBody(body));
+                }
+            });
+            progressBar.setVisibility(View.GONE);
+            cardView.setVisibility(View.VISIBLE);
+            long finish = System.currentTimeMillis();
+            long timeElapsed = finish - start;
+            Log.i(TAG, "timeElapsed to populate initial view: " + timeElapsed + " mS");
+            viewModel.setHeaderLoaded(true);
+        }
+    }
 
-				private void onGenerated(Palette palette) {
-					applyPalette(palette);
-				}
+    private void updateBackground(FloatingActionButton fab, Palette palette) {
+        int lightVibrantColor = palette.getLightVibrantColor(getResources().getColor(android.R.color.white));
+        int vibrantColor = palette.getVibrantColor(getResources().getColor(R.color.theme_primary_light));
+        fab.setRippleColor(lightVibrantColor);
+        fab.setBackgroundTintList(ColorStateList.valueOf(vibrantColor));
+    }
 
-				@Override
-				public void onSuccess() {
-					Bitmap bitmap = ((BitmapDrawable) photoView.getDrawable()).getBitmap();
-					Palette.from(bitmap).generate(this::onGenerated);
-					viewModel.getBody(data.getId()).observe(ArticleDetailActivity.this, body -> populateBody(body));
-				}
-			});
-			progressBar.setVisibility(View.GONE);
-			cardView.setVisibility(View.VISIBLE);
-			long finish = System.currentTimeMillis();
-			long timeElapsed = finish - start;
-			Log.i(TAG, "timeElapsed to populate initial view: " + timeElapsed + " mS");
-			viewModel.setHeaderLoaded(true);
-		}
-	}
+    public class SnackBarListener implements View.OnClickListener {
 
-	private void updateBackground(FloatingActionButton fab, Palette palette) {
-		int lightVibrantColor = palette.getLightVibrantColor(getResources().getColor(android.R.color.white));
-		int vibrantColor = palette.getVibrantColor(getResources().getColor(R.color.theme_primary_light));
-		fab.setRippleColor(lightVibrantColor);
-		fab.setBackgroundTintList(ColorStateList.valueOf(vibrantColor));
-	}
+        @Override
+        public void onClick(View v) {
+            Uri pictureUri = Uri.parse(data.getPhoto());
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, data.getTitle());
+            StringBuilder builder = new StringBuilder(data.getTitle()).append("\n")
+                    .append(data.getAuthor()).append("\n").append(publishedDate).append("\n\n")
+                    .append(data.getThumb()).append("\n").append(data.getPhoto());
+            shareIntent.putExtra(Intent.EXTRA_TEXT, builder.toString());
+            shareIntent.setType("text/plain");
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.shareArticle)));
+        }
+    }
 
 }
